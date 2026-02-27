ddffe48e859d97cfc67ab547c7ab16c745c79247
Sajjad <sajjadbh910@gmail.com>
2026-02-27 16:48:31 -0500

    The project structure

---
+++ src/accessControlledStructures/specifications/StackInterface.java
@@ -0,0 +1,10 @@
+package accessControlledStructures.specifications;
+
+public interface StackInterface<T> {
+
+    void push(T element);
+    void pop();
+    T top();
+    boolean isFull();
+    boolean isEmpty();
+}
