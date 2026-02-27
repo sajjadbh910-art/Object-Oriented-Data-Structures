bd385a67a9b96af4c91d811036bfb47d23267902
Sajjad <sajjadbh910@gmail.com>
2026-02-27 17:18:43 -0500

    Declare RuntimeExceptions in Stack interface methods

--- src/accessControlledStructures/exceptions/StackOverFlowException.java
+++ src/accessControlledStructures/exceptions/StackOverFlowException.java
@@ -1,7 +1,12 @@
 package accessControlledStructures.exceptions;
 
 public class StackOverFlowException extends RuntimeException {
+    public StackOverFlowException(){
+        super();
+    }
+
     public StackOverFlowException(String message) {
+
         super(message);
     }
 }
--- src/accessControlledStructures/specifications/StackInterface.java
+++ src/accessControlledStructures/specifications/StackInterface.java
@@ -1,10 +1,13 @@
 package accessControlledStructures.specifications;
 
+import accessControlledStructures.exceptions.StackOverFlowException;
+import accessControlledStructures.exceptions.StackUnderFlowException;
+
 public interface StackInterface<T> {
 
-    void push(T element); // adds an element to the stack.
-    void pop(); //removes an element from the stack.
-    T top(); // retrieves the last element inserted in the stack.
+    void push(T element)throws StackOverFlowException; // adds an element to the stack.
+    void pop()throws StackUnderFlowException; //removes an element from the stack.
+    T top()throws StackUnderFlowException; // retrieves the last element inserted in the stack.
     boolean isFull();//checks if the stack is full.
     boolean isEmpty();//checks if the stack is empty.
 
