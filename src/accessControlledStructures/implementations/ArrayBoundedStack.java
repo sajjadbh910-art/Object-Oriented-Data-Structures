7259dcbd98c308aa94581409ed8f362b19465d22
Sajjad <sajjadbh910@gmail.com>
2026-02-27 17:39:26 -0500

    defined two constructors to ArrayBoundedStack

--- src/accessControlledStructures/implementations/ArrayBoundedStack.java
+++ src/accessControlledStructures/implementations/ArrayBoundedStack.java
@@ -1,4 +1,47 @@
 package accessControlledStructures.implementations;
 
-public class ArrayBoundedStack {
+import accessControlledStructures.exceptions.StackOverFlowException;
+import accessControlledStructures.exceptions.StackUnderFlowException;
+import accessControlledStructures.specifications.StackInterface;
+
+public class ArrayBoundedStack<T> implements StackInterface<T> {
+
+
+    private final int DFCAP = 100; // default capacity of the array
+    private T[] elements;
+    private int topIndex;
+
+    //empty params constructor that initializes the Array with a default capacity
+       public ArrayBoundedStack(){
+           elements = (T[])new Object[DFCAP];
+    }
+
+    public ArrayBoundedStack(int numElements){
+           elements = (T[])new Object[numElements];
+    }
+
+    @Override
+    public void push(T element) throws StackOverFlowException {
+
+    }
+
+    @Override
+    public void pop() throws StackUnderFlowException {
+
+    }
+
+    @Override
+    public T top() throws StackUnderFlowException {
+        return null;
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
 }
