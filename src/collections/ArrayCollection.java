0bfd48a50713f7589f0c763770e2020eac99a0b0
Sajjad <sajjadbh910@gmail.com>
2026-02-27 21:42:31 -0500

    add two constructors and member variables to ArrayCollection

--- src/collections/ArrayCollection.java
+++ src/collections/ArrayCollection.java
@@ -1,4 +1,63 @@
 package collections;
 
-public class ArrayCollection {
+import accessControlledStructures.specifications.CollectionInterface;
+/*
+
+implements the Collection Interface using an array
+
+ */
+public class ArrayCollection<T> implements CollectionInterface<T> {
+
+    private final int DFCAP = 100;
+    private T[] elements;
+    private boolean isFound;
+    private int numElements;
+
+    public ArrayCollection(){
+        elements = (T[])new Object[DFCAP];
+    }
+
+    public ArrayCollection(int capacity){
+        elements = (T[])new Object[capacity];
+    }
+
+
+
+    private void find(T target){
+
+    }
+    @Override
+    public boolean add(T element) {
+        return false;
+    }
+
+    @Override
+    public T get() {
+        return null;
+    }
+
+    @Override
+    public boolean contains(T target) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(T target) {
+        return false;
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
